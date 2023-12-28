62353b3c13d5774e6080d568ce4d4c82fa9ab23f
braswelljr <braswellkenneth7@gmail.com>
2023-12-28 19:38:30 +0000

    fix: github workflows for CIs

--- www/next.config.js
+++ www/next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   output: 'export',
   distDir: process.env.NODE_ENV === 'development' ? 'out' : 'dist',
   images: { unoptimized: true },
   // for local development - (\\d{1,}) is for port number
   // async rewrites() {
-  //   return [{ source: '/api/parse', destination: 'http://localhost:8080/api/parse' }]
+  //   return [{ source: '/api/parse', destination: 'http://localhost:5000/api/parse' }]
   // }
 }
-
-module.exports = nextConfig
